4a9978f4583a4a6040d62166a38a7ca22d4ac3c4
HAEKWANG DAN <danhk0612@gmail.com>
2026-09-17 15:05:56 +0900

    Add mixer page code-behind

---
+++ src/DKRandomizeAIImagePromptGenerator/Views/MixerPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace DKRandomizeAIImagePromptGenerator.Views;
+
+public sealed partial class MixerPage : Page
+{
+    public MixerPage()
+    {
+        InitializeComponent();
+    }
+}
