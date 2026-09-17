40e44744d7071a3fa46e3ee307944d6dd0ed81eb
HAEKWANG DAN <danhk0612@gmail.com>
2026-09-17 15:06:50 +0900

    Add settings page code-behind

---
+++ src/DKRandomizeAIImagePromptGenerator/Views/SettingsPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace DKRandomizeAIImagePromptGenerator.Views;
+
+public sealed partial class SettingsPage : Page
+{
+    public SettingsPage()
+    {
+        InitializeComponent();
+    }
+}
